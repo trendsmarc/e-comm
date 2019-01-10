8ef7826da7f13e73857d0e83b4820d4e62bc0914
Marc Felice B. Asinas <mbasinas@trends.com.ph>
2019-01-10 17:40:32 +0800

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Item Ones'
+  });
+});
+
+router.get('/drinks', function (req, res, next) {
+  res.render('drinks', {
+
+  });
+});
+
+router.get('/condiments', function (req, res, next) {
+  res.render('condiments', {
+
+  });
+});
+
+router.get('/medicine', function (req, res, next) {
+  res.render('medicine', {
+
+  });
+});
+
+router.get('/toiletries', function (req, res, next) {
+  res.render('toiletries', {
+
+  });
+});
+
+
+module.exports = router;
